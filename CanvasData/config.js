89d66ebd50377e5213733f37941d6b16cf4434e6
benjaminselby <56523434+benjaminselby@users.noreply.github.com>
2020-11-13 10:54:05 +1030

    Update config.js

--- CanvasData/config.js
+++ CanvasData/config.js
@@ -1,14 +1,14 @@
 
 module.exports = {
   
-	saveLocation: '//wcfiler.woodcroft.sa.edu.au/gdrive$/canvas_data/dataFiles',
+	saveLocation: '//<HOSTNAME>/gdrive$/canvas_data/dataFiles',
   
-	unpackLocation: '//wcfiler.woodcroft.sa.edu.au/gdrive$/canvas_data/unpackedFiles', // if not defined, defaults to 'unpackedFiles'
+	unpackLocation: '//<HOSTNAME>/gdrive$/canvas_data/unpackedFiles', // if not defined, defaults to 'unpackedFiles'
   
 	apiUrl: 'https://api.inshosteddata.com/api',
   
-	key: "fc094ce68e55778ab9ed68572d3ba678f21b9a12",
-	secret: "708b74ebac4442d79c3f75cecbb43072cef770bc",
+	key: "<KEY>",
+	secret: "<SECRET>",
   
 	maxConnections: 200, // The maximum number of files allowed to be downloading simultaneously
 
